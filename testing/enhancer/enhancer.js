3f3ab6088f7c2cef849d64667be9573fdeb27eaa
RachelKolk <rach.m.obrien@gmail.com>
2019-03-05 09:55:10 -0500

    fail methods

--- testing/enhancer/enhancer.js
+++ testing/enhancer/enhancer.js
@@ -43,6 +43,14 @@ function success(item) {
 }
 
 function fail(item) {
+    if (item.enhancement < 15 && item.durability < 25) {
+        return {...item};
+    }
     
-    
+    const durability =
+        item.enhancement < 15 ? item.durability - 5 : item.durability - 10;
+    const enhancement = 
+        item.enhancement > 16 ? item.enhancement - 1 : item.enhancement;
+
+    return {...item, durability, enhancement};
 }
--- testing/enhancer/enhancer.spec.js
+++ testing/enhancer/enhancer.spec.js
@@ -8,18 +8,43 @@ describe('enhancer', () => {
   });
 
 
+  describe('success', () => {
+    it('enhancement is successful', () => {
+        expect(success({enhancement: 0}).enhancement).toBe(1);
+        expect(success({enhancement: 1}).enhancement).toBe(2);
+    });
 
-it('enhancement is successful', () => {
-    expect(success({enhancement: 0}).enhancement).toBe(1);
-    expect(success({enhancement: 1}).enhancement).toBe(2);
-    expect(success({enhancement: 1, type: 'armor'}).enhancement).toBe(2);
-    expect(success({enhancement: 'PEN'}).enhancement).toBe('PEN');
-    expect(success({enhancement: 'PRI'}).enhancement).toBe('DUO');
-    expect(success({enhancement: 'DUO'}).enhancement).toBe('TRI');
-    expect(success({enhancement: 'TRI'}).enhancement).toBe('TET');
-    expect(success({enhancement: 'TET'}).enhancement).toBe('PEN');
-    expect(success({enhancement: 15}).enhancement).toBe('PRI');
-    expect(success({enhancement: }))
+    it('type can be armor or weapon', () => {
+        expect(success({enhancement: 1, type: 'armor'}).enhancement).toBe(2);
+        expect(success({enhancement: 4, type: 'weapon'}).enhancement).toBe(5);
+    });
+
+    it('maximum enhancement is pen', () => {
+        expect(success({enhancement: 'PEN'}).enhancement).toBe('PEN');
+    })
+    
+    it('enhancement level of above 15 - 16 is equal to PRI', () => {
+        expect(success({enhancement: 15}).enhancement).toBe('PRI');
+    });
+
+    it('enhancement level of above 15 - 17 is equal to DUO', () => {
+        expect(success({enhancement: 'PRI'}).enhancement).toBe('DUO');
+    })
+
+    it('enhancement level of above 15 - 18 is equal to TRI', () => {
+        expect(success({enhancement: 'DUO'}).enhancement).toBe('TRI');
+    })
+    
+    it('enhancement level of above 15 - 19 is equal to TET', () => {
+        expect(success({enhancement: 'TRI'}).enhancement).toBe('TET');
+    })
+    
+    it('enhancement level of above 15 - 20 is equal to PEN', () => {
+        expect(success({enhancement: 'TET'}).enhancement).toBe('PEN');
+    })
+    
+    
+    
 }); 
 
 
